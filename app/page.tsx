e8ccd0818fe7254feac1f2021f0a645edb10b06d
leo <leo.coyere94@gmail.com>
2025-03-17 13:29:56 +0100

    Improve UI

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,14 @@ import { getImages } from '../db/queries/select';
 
 export default async function Home() {
   const images = await getImages();
-  console.log(images);
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      
+    <div className="flex flex-wrap gap-4">
+      {[...images, ...images, ...images].map((image, index) => (
+        <div key={image.id + index} className="w-48">
+          <img src={image.url} alt={`image-${image.id}`} />
+        </div>
+      ))}
     </div>
   );
 }
